e77c25c20cf113b1fe1c5eb271a9e1ca7f868410
吴棒棒 <302857194@qq.com>
2019-04-02 16:25:37 +0800

    代码提交

---
+++ SwiftDemo/class/mine/view/TYNoLogInHeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  TYNoLogInHeaderView.swift
+//  SwiftDemo
+//
+//  Created by wbb on 2019/4/2.
+//  Copyright © 2019 cjh. All rights reserved.
+//
+
+import UIKit
+import IBAnimatable
+
+class TYNoLogInHeaderView: UIView {
+
+    @IBOutlet weak var phone: UIImageView!
+    @IBOutlet weak var weChat: UIImageView!
+    @IBOutlet weak var qq: UIImageView!
+    @IBOutlet weak var xinlang: UIImageView!
+    @IBOutlet weak var backView: UIView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
